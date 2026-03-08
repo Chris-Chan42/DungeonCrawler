0b34f578fbcc7a9585381608c55fb0eeb12caaaa
Chris-Chan42 <157049369+Chris-Chan42@users.noreply.github.com>
2026-03-08 15:33:33 -0700

    Add files via upload
    Created an enemy system with 4 types of enemies and 1 boss type enemy, created a Flee mechanic that takes you to the previous room, added a win condition from beating the boss type enemy.

--- DungeonCrawler/Board/Creature.cs
+++ DungeonCrawler/Board/Creature.cs
@@ -4,11 +4,27 @@
     {
         public string Name { get; set; }
         public int Health { get; set; }
+        public int Damage { get; set; }
+        public string Type { get; set; }
+        public bool IsBoss { get; set; }
 
-        public Creature(string name, int health)
+
+        public Creature(string name, int health, int damage, string type, bool boss)
         {
             Name = name;
             Health = health;
+            Damage = damage;
+            Type = type;
+            IsBoss = boss;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+        public bool IsDead()
+        {
+            return Health <= 0;
         }
     }
 }
--- DungeonCrawler/Program.cs
+++ DungeonCrawler/Program.cs
@@ -7,11 +7,13 @@ namespace DungeonCrawler
     {
         static void Main(string[] args)
         {
+            int playerHealth = 100;
+            int playerDamage = 15;
             World world = new World(); // 10x10 grid
 
             Console.WriteLine("Welcome to DungeonCrawler!");
             Console.WriteLine("You are in a dungeon. Explore and survive!");
-            Console.WriteLine("Commands: Move N.E.S.W., look, quit");
+            Console.WriteLine("Commands: Move N/E/S/W, attack, look, flee, quit");
             Console.WriteLine("[P]\tPlayer location\r\n[I]\tRoom has item(s)\r\n[C]\tRoom has creature(s)\r\n[B]\tRoom has both item & creature\r\n[.]\tRoom has been visited and is cleared (items/creatures removed or empty)\r\n[ ]\tEmpty space");
             world.DisplayMap();
             Console.WriteLine(world.Look());
@@ -38,6 +40,60 @@ namespace DungeonCrawler
                 {
                     break;
                 }
+                
+                else if (command == "attack")
+                { 
+                    var room = world.GetCurrentRoom();
+                    if (room.Creatures.Count == 0) 
+                    { 
+                        Console.WriteLine("There is nothing to attack!");
+                        continue;
+                    }
+
+                    var creature = room.Creatures[0];
+
+                    if (creature.Type == "bomb")
+                    {
+                        Console.WriteLine("You strike the bomb...");
+                        Console.WriteLine("BOOM!!! The bomb explodes instantly!");
+                    }
+
+                    creature.Health -= playerDamage;
+
+                    Console.WriteLine($"You attack the {creature.Name} for {playerDamage} damage!");
+                    if (creature.Health <= 0) 
+                    { 
+                        Console.WriteLine($"You have defeated the {creature.Name}!");
+
+                        world.RemoveCreatureFromCurrentRoom(creature);
+
+                        if (creature.IsBoss)
+                        {
+                            Console.WriteLine("You defeated the dungeon boss!");
+                            Console.WriteLine("YOU WIN!");
+                            Environment.Exit(0);
+                        }
+                    }
+
+                    foreach (var enemy in room.Creatures)
+                    {
+                        Console.WriteLine($"The {enemy.Name} attacks you for {enemy.Damage} damage!");
+                        playerHealth -= enemy.Damage;
+                    }
+
+                    Console.WriteLine($"Player HP: {playerHealth}");
+
+                    if (playerHealth <= 0) 
+                    { 
+                        Console.WriteLine("You have been defeated! Game Over.");
+                        break;
+                    }
+                }
+                else if (command == "flee")
+                {
+                    world.Flee();
+                }
+
                 else
                 {
                     Console.WriteLine("Unknown command.");
